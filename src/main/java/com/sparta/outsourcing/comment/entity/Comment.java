585b4680928d60fdad3eb561d82cfa9a330926fa
hoon <dotofi@naver.com>
2024-06-20 13:19:12 +0900

    fix : Comment Entity User 연관 관계 수정

--- src/main/java/com/sparta/outsourcing/comment/entity/Comment.java
+++ src/main/java/com/sparta/outsourcing/comment/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.sparta.outsourcing.comment.entity;
 
+import com.sparta.outsourcing.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +14,15 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "comment", nullable = false)
     private String comment;
-
-    private Long userId;
 
 //    @ManyToOne
 //    @JoinColumn(name = "board_id")
 //    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
