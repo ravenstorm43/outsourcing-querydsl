6e75ecb75bdd4c66bae6a3baa85450440e25fe1d
Jinny <152242318+co-jinny@users.noreply.github.com>
2024-06-21 13:25:45 +0900

    게시글 CRUD 중 read 및 수

--- src/main/java/com/sparta/outsourcing/board/controller/BoardController.java
+++ src/main/java/com/sparta/outsourcing/board/controller/BoardController.java
@@ -1,30 +1,47 @@
 package com.sparta.outsourcing.board.controller;
 
 import com.sparta.outsourcing.board.dto.BoardCreateRequest;
+import com.sparta.outsourcing.board.dto.BoardDetailResponseDto;
+import com.sparta.outsourcing.board.dto.BoardListResponseDto;
 import com.sparta.outsourcing.board.service.BoardService;
 import com.sparta.outsourcing.security.UserDetailsImpl;
 import com.sparta.outsourcing.user.entity.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/boards")
 @RequiredArgsConstructor
 public class BoardController {
-    public final BoardService boardService;
+    private final BoardService boardService; //캡슐화
 
     // 게시글 작성
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody BoardCreateRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<String> create(@Valid  @RequestBody BoardCreateRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
         System.out.println(userDetails.getUser().getUserUid());
         System.out.println(userDetails.getUser().getUsername());
         boardService.createBoard(request, user);
         return ResponseEntity.status(201).body("게시글이 작성되었습니다.");
     }
+
+    // 게시글 전체 조회 : 누구든 조회 가능
+    @GetMapping
+    public ResponseEntity<BoardListResponseDto> getBoardList(
+            @RequestParam(defaultValue = "1") int page, //페이지 번호 파라미터
+            @RequestParam(defaultValue = "5") int size) { //페이지당 갯수 파라미터
+        BoardListResponseDto response = boardService.getAllBoards(page -1, size);
+        return ResponseEntity.ok(response);
+    }
+
+    // 게시글 선택 조회 : 누구든 조회 가능, 하나의 게시글만 조회
+    @GetMapping("/{boardId}")
+    public ResponseEntity<BoardDetailResponseDto> getBoardDetail(@PathVariable Long boardId) {
+        BoardDetailResponseDto response = boardService.getBoardDetail(boardId);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/com/sparta/outsourcing/board/repository/BoardRepository.java
+++ src/main/java/com/sparta/outsourcing/board/repository/BoardRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.outsourcing.board.repository;
 
 import com.sparta.outsourcing.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    //최신순으로 게시물 페이징
+    Page<Board> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
